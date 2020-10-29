c5d9700dce252e24f24e70f31797563835dfd6be
Charles Desiderio <charlesdesiderio@MacBook-of-the-Dead.local>
2020-10-29 08:50:42 -0500

    Database update

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 3000;
 //Database
 //___________________
 // How to connect to the database either via heroku or locally
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/'+ `heroku-test`;
+const MONGODB_URI = process.env.MONGODB_URI + `heroku-test`;
 
 // Connect to Mongo
 mongoose.connect(MONGODB_URI ,  { useNewUrlParser: true});
